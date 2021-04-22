211a6c018a943aba30497eac7190a159f800425a
Mohitos <dipmollick013@gmail.com>
2021-04-22 17:46:09 +0600

    file initiated

---
+++ main.js
@@ -0,0 +1,31 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+
+    var close_btn = document.getElementById("close-nav");
+    var navbarNav = document.getElementById("navbarNav");
+    var navbar_toggler = document.getElementById("navbar-toggler");
+
+    close_btn.addEventListener("click", function() {
+        navbarNav.style.display = "none";
+    });
+
+    navbar_toggler.addEventListener("click", function() {
+        navbarNav.style.display = "block";
+    });
+
+})();
